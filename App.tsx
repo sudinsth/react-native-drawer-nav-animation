6a2412250eebd084d82680d277ea92be659bb312
sudinsth <sudinsth1997@gmail.com>
2024-03-25 04:58:50 +0545

    add: drawer animation

--- App.tsx
+++ App.tsx
@@ -1,118 +1,140 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import * as React from 'react';
+import {View, Text, Alert, TouchableOpacity} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+  DrawerActions,
+  NavigationContainer,
+  createNavigationContainerRef,
+  useNavigation,
+} from '@react-navigation/native';
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+  useDrawerProgress,
+} from '@react-navigation/drawer';
+import Animated, {interpolate, useAnimatedStyle} from 'react-native-reanimated';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function DrawerSceneWrapper({children}: any) {
+  const drawerProgress = useDrawerProgress();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {scale: interpolate(drawerProgress.value, [0, 1], [1, 0.8], 'clamp')},
+    ],
+    overflow: 'hidden',
+    borderRadius: interpolate(drawerProgress.value, [0, 1], [0, 10], 'clamp'),
+  }));
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Animated.View
+      style={[animatedStyle, {flex: 1, backgroundColor: 'crimson'}]}>
+      {children}
+    </Animated.View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+function Feed() {
+  const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <DrawerSceneWrapper>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}>
+        <TouchableOpacity
+          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+          <Text>Feed Screen</Text>
+        </TouchableOpacity>
+      </View>
+    </DrawerSceneWrapper>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+function Article() {
+  const navigation = useNavigation();
 
-export default App;
+  return (
+    <DrawerSceneWrapper>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}>
+        <TouchableOpacity
+          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+          <Text>Article Screen</Text>
+        </TouchableOpacity>
+      </View>
+    </DrawerSceneWrapper>
+  );
+}
+
+function CustomDrawerContent(props: any) {
+  const progress = useDrawerProgress();
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: interpolate(
+          progress.value,
+          [0, 1],
+          [-100, 0],
+          'clamp', // Use extrapolate property instead of passing 'clamp' as a separate argument
+        ),
+      },
+    ],
+  }));
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <Animated.View style={animatedStyle}>
+        <DrawerItemList {...props} />
+        <DrawerItem label="Help" onPress={() => Alert.alert('Link to help')} />
+      </Animated.View>
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'slide',
+        overlayColor: 'transparent',
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: 'green',
+        drawerStyle: {
+          backgroundColor: 'crimson',
+          width: '50%',
+        },
+        sceneContainerStyle: {
+          backgroundColor: 'crimson',
+        },
+      }}
+      drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <MyDrawer />
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
+}
